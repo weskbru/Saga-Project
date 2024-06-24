e0729e30c51e4180a08c8c6fe74a44aae792164f
Wesley <Wesley@DESKTOP-BH6M3VH>
2024-06-23 23:17:08 -0300

    criando o projeo spring

---
+++ feedback-system/src/main/java/com/exemplo/feedbacksystem/FeedbackSystemApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.feedbacksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackSystemApplication.class, args);
+	}
+
+}
---
+++ feedback-system/src/test/java/com/exemplo/feedbacksystem/FeedbackSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.feedbacksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
